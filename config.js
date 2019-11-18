c8708a4967dbe65ecc5aec783c5e5ab4b4b72a7c
AbdulRehman197 <abdulrehman19761@gmail.com>
2019-11-18 17:59:11 +0500

    init

---
+++ config.js
@@ -0,0 +1,48 @@
+
+const config = {
+	"gatsby": {
+		"pathPrefix": "/",
+		"siteUrl": "https://learn.hasura.io",
+		"gaTrackingId": null
+	},
+	"header": {
+		"logo": "https://miro.medium.com/max/1000/1*G9aVAI3aezHLw_JsiCfB1Q.jpeg",
+		"logoLink": "https://learn.hasura.io",
+		"title": "Gatsby-theme-apollo-doc",
+		"githubUrl": "https://github.com/hasura/gatsby-gitbook-boilerplate",
+		"helpUrl": "",
+		"tweetText": "abdulrehman",
+		"links": [
+			{ "text": "", "link": ""}
+		],
+		"search": {
+			"enabled": true,
+			"indexName": "Gatsby",
+			"algoliaAppId": "IOA8FSVX1G",
+			"algoliaSearchKey": "cdf8acd17bad3d2cbfc45b38a0c6ba5d",
+			"algoliaAdminKey": "9e5d9c6c52a3ce127b3ec28d8de708bb"
+		}
+	},
+	"sidebar": {
+		"forcedNavOrder": [
+			"/introduction",
+    		"/codeblock"
+		],
+		"links": [
+			{ "text": "Hasura", "link": "https://hasura.io"},
+		],
+		"frontline": false,
+		"ignoreIndex": true,
+	},
+	"siteMetadata": {
+		"title": "Gatsby-theme-apollo-doc ",
+		"description": "Documentation Gatsby-theme-apollo-doc",
+		"ogImage": null,
+		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
+		"favicon": "https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
+	},
+};
+
+module.exports = config;
+
+// https://miro.medium.com/max/1000/1*G9aVAI3aezHLw_JsiCfB1Q.jpeg
